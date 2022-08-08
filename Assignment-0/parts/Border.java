3436519f205a91e2772942b12a8838f1dc6f6429
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2022-08-08 10:25:34 +0530

    gitignore weird

--- Assignment-0/parts/Border.java
+++ Assignment-0/parts/Border.java
@@ -1,7 +1,7 @@
 package parts;
  
 public class Border {
-    // dummy class (?????)
+    // dummy class (?????) not sure if need to make Clock class either
     public static int width;
     public Border(int d, int L){
         width = d;
