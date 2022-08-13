41934e7656a6d3ff31f9193ed50b21c02992be5f
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2022-08-13 20:42:38 +0530

    palindrome down!!!

---
+++ Assignment-1/supporting_files/palindrome.asm
@@ -0,0 +1,20 @@
+	.data
+a:
+	12343
+	.text
+main:
+	load %x0, $a, %x3
+	addi %x0, 0, %x4
+	add %x0, %x3, %x5
+loop:
+	divi %x5, 10, %x5
+	muli %x4, 10, %x4
+	add %x4, %x31, %x4
+	bne %x5, %x0, loop
+out:
+	beq %x4, %x3, yespal
+	subi %x0, 1, %x10
+	end
+yespal:
+	addi %x0, 1, %x10
+	end
--- Assignment-1/supporting_files/palindrome_template.asm
+++
@@ -1,7 +0,0 @@
-	.data
-a:
-	10
-	.text
-// your code here to check if 'a' is a palindrome
-// you may change the value of 'a'
-// remove these comments!
