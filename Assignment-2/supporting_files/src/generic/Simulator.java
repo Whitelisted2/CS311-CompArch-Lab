4314271cd7aee212b774b44ff0efba7036257c22
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2022-09-07 13:50:50 +0530

    skeleton

--- Assignment-2/supporting_files/src/generic/Simulator.java
+++ Assignment-2/supporting_files/src/generic/Simulator.java
@@ -1,11 +1,15 @@
 package generic;
 
-import java.io.FileInputStream;
+import java.io.*;
+import java.nio.ByteBuffer;
 import generic.Operand.OperandType;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Simulator {
 
-	public static Map<String,String> opTable = new HashMap<>(){
+	public static Map<String,String> opTable = new HashMap<>(){{
 		put("add", "00000");
 		put("sub", "00010");
 		put("mul", "00100");
@@ -36,7 +40,7 @@ public class Simulator {
 		put("bgt", "11100");
 		put("jmp", "11000");
 
-	}
+	}};
 		
 	static FileInputStream inputcodeStream = null;
 	
@@ -47,17 +51,91 @@ public class Simulator {
 		ParsedProgram.printState();
 	}
 	
+
 	public static void assemble(String objectProgramFile)
 	{
-		//TODO your assembler code
-		//1. open the objectProgramFile in binary mode
-		//2. write the firstCodeAddress to the file
-		//3. write the data to the file
-		//4. assemble one instruction at a time, and write to the file
+		FileOutputStream weno;
+		try{
+			//1. open the objectProgramFile in binary mode
+			weno = new FileOutputStream(objectProgramFile);
+			BufferedOutputStream mech = new BufferedOutputStream(weno);
+
+			//2. write the firstCodeAddress to the file
+			byte[] codeAddress = ByteBuffer.allocate(4).putInt(ParsedProgram.firstCodeAddress).array();
+			mech.write(codeAddress);
+
+			//3. write the data to the file
+			for(int i=0; i<ParsedProgram.data.size(); i++){
+				byte[] lettuce = ByteBuffer.allocate(4).putInt(ParsedProgram.data.get(i)).array();
+				mech.write(lettuce);
+			}
+
+			//4. assemble one instruction at a time, and write to the file
+			for(int i=0; i<ParsedProgram.code.size(); i++){
+				String bisaun = "";
+				String opType = ParsedProgram.code.get(i).toString();
+				if(opType.equals("jmp")){
+					bisaun += (opTable.get(ParsedProgram.code.get(i).operationType.toString()));
+					bisaun += "00000";
+					int destAddr = 0;
+					if(ParsedProgram.code.get(i).destinationOperand.toString().equals("Label")){
+						destAddr = ParsedProgram.symtab.get(ParsedProgram.code.get(i).destinationOperand.labelValue);
+					}
+					else if(ParsedProgram.code.get(i).destinationOperand.toString().equals("Immediate")){
+						destAddr = ParsedProgram.code.get(i).destinationOperand.value;
+					}
+					int pc = ParsedProgram.code.get(i).programCounter;
+					int offset = destAddr - pc;
+					if(offset>=0){
+						String binRep = Integer.toBinaryString(offset);
+						int binOffset = 22- binRep.length();
+						String concata = "";
+						if(binOffset!=0){
+							for(int j=0;j<binOffset;j++){
+								concata.concat("0");
+							}
+						}
+						bisaun.concat(concata);
+						bisaun.concat(binRep);
+					}
+					if(offset<0)
+					{
+						String binRep = Integer.toBinaryString(offset);
+						String subRep = binRep.substring(10, 32);
+						bisaun.concat(subRep);
+					}
+				}
+				else if(opType.equals("load")||opType.equals("store")){
+
+				}
+				else if(opType.equals("add") || opType.equals("sub") || opType.equals("mul") || opType.equals("div") || opType.equals("and") || opType.equals("or") || opType.equals("xor") || opType.equals("slt") || opType.equals("sll") || opType.equals("srl") || opType.equals("sra")){
+
+				}
+				else if(opType.equals("addi") || opType.equals("subi") || opType.equals("muli") || opType.equals("divi") || opType.equals("andi") || opType.equals("ori") || opType.equals("xori") || opType.equals("slti") || opType.equals("slli") || opType.equals("srli") || opType.equals("srai")){
+
+				}
+				else if(opType.equals("beq") || opType.equals("bgt") || opType.equals("bne") || opType.equals("blt")){
+
+				}
+				else if(opType.equals("end")){
+					
+				}
+			}
+			
+
+
+
+
+		} catch(FileNotFoundException e){
+			e.printStackTrace();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+
 		//5. close the file
 	}
 	
-}
+
 
 
 	public static Map<String, String> getOpTable() {
@@ -66,4 +144,4 @@ public class Simulator {
 
 	public static void setOpTable(Map<String, String> opTable) {
 		Simulator.opTable = opTable;
-	}
+	}}
