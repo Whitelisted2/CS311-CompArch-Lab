93a2319c9de6a62576b073f9e5dfc1dd598bc01e
Devdatt N <devblixt@gmail.com>
2022-09-20 12:52:51 +0530

    half bake

--- Assignment-3/supporting_files/src/processor/pipeline/Execute.java
+++ Assignment-3/supporting_files/src/processor/pipeline/Execute.java
@@ -1,5 +1,7 @@
 package processor.pipeline;
-
+import generic.Instruction;
+import generic.Instruction.OperationType;
+import generic.Operand.OperandType;
 import processor.Processor;
 
 public class Execute {
@@ -7,6 +9,7 @@ public class Execute {
 	OF_EX_LatchType OF_EX_Latch;
 	EX_MA_LatchType EX_MA_Latch;
 	EX_IF_LatchType EX_IF_Latch;
+	private boolean b;
 	
 	public Execute(Processor containingProcessor, OF_EX_LatchType oF_EX_Latch, EX_MA_LatchType eX_MA_Latch, EX_IF_LatchType eX_IF_Latch)
 	{
@@ -16,9 +19,121 @@ public class Execute {
 		this.EX_IF_Latch = eX_IF_Latch;
 	}
 	
+
 	public void performEX()
 	{
-		//TODO
+		/* blabla
+		 * 
+		 */
+		if(OF_EX_Latch.isEX_enable()){
+			Instruction instruction = OF_EX_Latch.getInstruction();
+			EX_MA_Latch.setInstruction(instruction);
+			OperationType opType = instruction.getOperationType();
+			int nowPc = containingProcessor.getRegisterFile().programCounter -1;
+			int aluResult = 0;
+			b = opType.equals("addi") || opType.equals("subi") || opType.equals("muli") || opType.equals("divi") || opType.equals("andi") || opType.equals("ori") || opType.equals("xori") || opType.equals("slti") || opType.equals("slli") || opType.equals("srli") || opType.equals("srai") || opType.equals("load") || opType.equals("store");
+			if(b){
+				int rs1 = containingProcessor.getRegisterFile().getValue(instruction.getSourceOperand1().getValue());
+				int immed = instruction.getDestinationOperand().getValue();
+				switch(opType){
+					case addi:
+						aluResult = rs1 + immed;
+						break;
+					case subi:
+						aluResult = rs1 - immed;
+						break;
+					case muli:
+						aluResult = rs1*immed;
+						break;
+					case divi:
+						aluResult = rs1/immed;
+						break;
+					case andi:
+						aluResult = rs1&immed;
+						break;
+					case ori:
+						aluResult = rs1 | immed;
+						break;
+					case xori:
+						aluResult = rs1 ^ immed;
+						break;
+					case slti:
+						aluResult = 0;
+						if(immed > rs1) aluResult = 1;
+						break;
+					case slli: 
+						aluResult = rs1 << immed;
+						break;
+					case srli: 
+						aluResult = rs1 >>> immed;
+						break;
+					case srai: 
+						aluResult = rs1 >> immed;
+						break;
+					case load:
+						aluResult = rs1 + immed;
+						break;
+					case store:
+						aluResult = rs1 + immed;
+						break;
+					default: 
+						System.out.print("Issue detected in Execute.java, switch(OpType) for R2I");
+				}
+			else if(opType.equals("add") || opType.equals("sub") || opType.equals("mul") || opType.equals("div") || opType.equals("and") || opType.equals("or") || opType.equals("xor") || opType.equals("slt") || opType.equals("sll") || opType.equals("srl") || opType.equals("sra")){
+				int rs1 = containingProcessor.getRegisterFile().getValue(instruction.getSourceOperand1().getValue());
+				int rs2 = containingProcessor.getRegisterFile().getValue(instruction.getSourceOperand2().getValue());
+				int rd = containingProcessor.getRegisterFile().getValue(instruction.getDestinationOperand().getValue());
+				switch(opType){
+					case add:
+						aluResult = rs1 + rs2;
+						break;
+					case sub:
+						aluResult = rs1 - rs2;
+						break;
+					case mul:
+						aluResult = rs1 * rs2;
+						break;
+					case div:
+						aluResult = rs1 / rs2;
+						break;
+					case and: 
+						aluResult = rs1 & rs2;
+						break;
+					case or:
+						aluResult = rs1 | rs2;
+						break;
+					case xor:
+						aluResult = rs1 ^ rs2;
+						break;
+					case slt:
+						aluResult = 0;
+						if(rs2 > rs1) aluResult = 1;
+						break;
+					case sll:
+						aluResult = rs1 << rs2;
+						break;
+					case srl:
+						aluResult = rs1 >>> rs2;
+						break;
+					case sra:
+						aluResult = rs1 >> rs2;
+						break;
+					default:
+						System.out.print("Issue detected in R3 type switch");
+				}
+			else if(opType.equals("jmp")){
+				OperandType jmpType = instruction.getDestinationOperand().getOperandType();
+				int immed = 0;
+				if(jmpType == OperandType.Immediate)
+					immed = instruction.getDestinationOperand().getValue();
+				else immed = containingProcessor.getRegisterFile().getValue(instruction.getDestinationOperand().getValue());
+				aluResult = immed + nowPc;
+				EX_IF_Latch.setIsBranch_enable(true,aluResult);
+			}
+			}
+			}
+		}
+
 	}
 
 }
