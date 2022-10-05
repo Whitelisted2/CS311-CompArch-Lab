9a726aa12d4c2d16b9294a7e978b0279fcd8c989
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2022-10-06 00:16:12 +0530

    a4 stuff

---
+++ Assignment-4/supporting_files/src/processor/pipeline/EX_IF_LatchType.java
@@ -0,0 +1,52 @@
+package processor.pipeline;
+
+public class EX_IF_LatchType {
+	
+	boolean IsBranch_enable;
+	int PC;
+
+	// constructor
+	public EX_IF_LatchType()
+	{
+		IsBranch_enable = false;
+	}
+
+	public EX_IF_LatchType(boolean isBranch_enable) // we may want to set multiplexer to 0, so no need to change pC
+	{
+		IsBranch_enable = isBranch_enable;
+	}
+
+	public EX_IF_LatchType(boolean isBranch_enable, int pC)
+	{
+		IsBranch_enable = isBranch_enable;
+		PC = pC;
+	}
+
+	// enable
+	public boolean getIsBranch_enable()
+	{
+		return IsBranch_enable;
+	}
+	
+	public void setIsBranch_enable(boolean isBranch_enable)
+	{
+		this.IsBranch_enable = isBranch_enable;
+	}
+
+	public void setIsBranch_enable(boolean isBranch_enable, int pC)
+	{
+		IsBranch_enable = isBranch_enable;
+		this.PC = pC;
+	}
+
+	// PC
+	public int getPC()
+	{
+		return PC;
+	}
+
+	public void setPC(int pC)
+	{
+		PC = pC;
+	}
+}
