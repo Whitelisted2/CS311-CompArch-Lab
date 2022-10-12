4cdcb64a9bb0e4a30d512029302548b37b101346
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2022-10-12 14:24:20 +0530

    Revert "attained post-viva clarity"
    This reverts commit 83ffa990f2a4fdabe496f6dfb77a328a15fdb4b1.

---
+++ Assignment-2/supporting_files/test_cases/palindrome.asm
@@ -0,0 +1,21 @@
+	.data
+a:
+	102
+	.text
+main:
+	load %x0, $a, %x3
+	addi %x0, 0, %x4
+	blt %x3, %x0, out
+	add %x0, %x3, %x5
+loop:
+	divi %x5, 10, %x5
+	muli %x4, 10, %x4
+	add %x4, %x31, %x4
+	bne %x5, %x0, loop
+out:
+	beq %x4, %x3, yespal
+	subi %x0, 1, %x10
+	end
+yespal:
+	addi %x0, 1, %x10
+	end
